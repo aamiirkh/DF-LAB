f153b0740be6e699f19b1ba23d2471c7d9542f26
aamirkh123 <83310209+aamirkh123@users.noreply.github.com>
2022-05-26 15:56:40 -0400

    Update yara_dir.py

--- yara_dir.py
+++ yara_dir.py
@@ -3,21 +3,20 @@ import os
 import yara
 
 def dir_search(file, rule):
-    if(os.path.isdir(file)):
-        directory = os.listdir(file)
-        for directories in directory:
-            file += "/" + directories
-            dir_search(file, rule)
-    else:
-        if(rule.match(file)):
-            print("Rule Matched.")
-        else:
-            print("Rule Not Matched.")
+	if(os.path.isdir(file)):
+		directory = os.listdir(file)
+		for directories in directory:
+			dir_search(file+"/"+directories, rule)
+	else:
+		if(rule.match(file)):
+			print("Rule Matched = "+ file)
 
 
 if __name__=="__main__":
-    parser=argparse.ArgumentParser() 
-    parser.add_argument('-f', help = 'argument for file')
-    parser.add_argument('-r', help = 'argument for rule file')
-    args = parser.parse_args()
-    dir_search(args.f, yara.compile(args.r))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path',help='Add the path you want the rules to match')
+    parser.add_argument('-r',help='yara rule here.')
+    arg = parser.parse_args()
+    testing = arg.path
+    rules=yara.compile(filepath=arg.r)
+    dir_search(testing, rules)
