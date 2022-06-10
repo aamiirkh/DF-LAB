235ee0adad8c25f11cef978e7a8371f5c1b0a5d5
aamirkh123 <83310209+aamirkh123@users.noreply.github.com>
2022-06-11 01:16:22 +0500

    Update automation.py

--- automation.py
+++ automation.py
@@ -7,8 +7,9 @@ s = s.replace(',}','}')
 data = json.loads(s)
 for i in data:
 	for j in data[i]:
-		print(data[i][j])
-	print("\n")
+		if(j == 'ssh'):
+			print(j, " : ", data[i][j])
+		print("\n")
 f.close()
 
 #incomplete
